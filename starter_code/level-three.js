31bbfd945ef234d5b33ef02b9414c41aa9ef38ce
Matt <gorajmatt@gmail.com>
2021-10-21 21:51:05 -0400

    Started Level Three

--- starter_code/level-three.js
+++ starter_code/level-three.js
@@ -6,9 +6,9 @@ let score = 0;
 let arenaEl = document.querySelector('.js-arena');
 let scoreEl = document.querySelector('.js-score');
 
-arenaEl.addEventListener('click', function() {
+arenaEl.addEventListener('click', function(e) {
   if ( e.target.classList.contains('js-ball') ) {
-    score += parseInt(e.target.dataset.increment);
+    score += data-increment;
     if ( score < 100 ) {
       scoreEl.innerText = score;
     } else {
